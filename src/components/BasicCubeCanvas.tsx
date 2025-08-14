9f1ab89ba94f2f6da7989c52ed6d000a84a796a7
kevinluo <luokev1@gmail.com>
2025-08-14 16:36:45 -0400

    orbit wip

--- src/components/BasicCubeCanvas.tsx
+++ src/components/BasicCubeCanvas.tsx
@@ -1,29 +1,27 @@
 import { useRef, useEffect } from "react";
 import * as THREE from "three"
-import { OrbitControls } from "three/addons/controls/OrbitControls.js"
 
 
-export default function ThreeCanvas() {
+
+export default function BasicCubeCanvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   
-  
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  const renderer = new THREE.WebGLRenderer()
 
   const geometry = new THREE.BoxGeometry(1,1,1)
   const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
   const cube = new THREE.Mesh( geometry, material )
   
-  
-
-  useEffect(() => {
-    const renderer = new THREE.WebGLRenderer()
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
-    const animate = () => {
+  const animate = () => {
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
     renderer.render(scene, camera)
   }
+
+  useEffect(() => {
     // we'll fill this in next steps
     renderer.setSize(window.innerWidth, window.innerHeight)
 
