26bc7d12d186a074cf8cfd69c157efffbd7ca38d
kevinluo <luokev1@gmail.com>
2025-08-14 15:57:54 -0400

    working orbiting cube

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -1,10 +1,35 @@
 import { useRef, useEffect } from "react";
+import * as THREE from "three"
+
+
 
 export default function ThreeCanvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  const renderer = new THREE.WebGLRenderer()
+
+  const geometry = new THREE.BoxGeometry(1,1,1)
+  const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+  const cube = new THREE.Mesh( geometry, material )
+  
+  const animate = () => {
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+  }
 
   useEffect(() => {
     // we'll fill this in next steps
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+  document.body.appendChild(renderer.domElement)
+    scene.add(cube)
+    camera.position.z = 5
+
+    renderer.setAnimationLoop(animate)
   }, []);
 
   return (
